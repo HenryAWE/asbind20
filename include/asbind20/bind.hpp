6e4699494a2a4021b7cb7316f474f29682ceece9
AWE Henry <mouth11or12@outlook.com>
2024-04-08 20:57:32 +0800

    Method wrapper

--- include/asbind20/bind.hpp
+++ include/asbind20/bind.hpp
@@ -6,10 +6,47 @@
 #include <cassert>
 #include <type_traits>
 #include <string>
+#include <tuple>
 #include <angelscript.h>
 
 namespace asbind20
 {
+namespace detail
+{
+    template <typename T>
+    class function_traits;
+
+    template <typename R, typename... Args>
+    class function_traits<R(Args...)>
+    {
+    public:
+        using type = R(Args...);
+        using return_type = R;
+        using args_type = std::tuple<Args...>;
+    };
+
+    template <typename R, typename... Args>
+    class function_traits<R (*)(Args...)>
+    {
+    public:
+        using type = R(Args...);
+        using return_type = R;
+        using args_type = std::tuple<Args...>;
+    };
+
+    template <typename T, typename Class>
+    concept is_this_arg =
+        std::is_same_v<T, Class*> ||
+        std::is_same_v<T, const Class*>;
+} // namespace detail
+
+template <asECallConvTypes CallConv>
+struct call_conv_t
+{};
+
+template <asECallConvTypes CallConv>
+constexpr inline call_conv_t<CallConv> call_conv;
+
 template <typename T>
 class value_class
 {
@@ -143,9 +180,67 @@ public:
         return *this;
     }
 
+    template <typename Fn>
+    value_class& method(const char* decl, Fn&& fn, call_conv_t<asCALL_CDECL_OBJFIRST>)
+    {
+        int r = m_engine->RegisterObjectMethod(
+            m_name,
+            decl,
+            asFunctionPtr(fn),
+            asCALL_CDECL_OBJFIRST
+        );
+        assert(r >= 0);
+
+        return *this;
+    }
+
+    template <typename Fn>
+    value_class& method(const char* decl, Fn&& fn, call_conv_t<asCALL_CDECL_OBJLAST>)
+    {
+        int r = m_engine->RegisterObjectMethod(
+            m_name,
+            decl,
+            asFunctionPtr(fn),
+            asCALL_CDECL_OBJLAST
+        );
+        assert(r >= 0);
+
+        return *this;
+    }
+
+    template <typename R, typename... Args>
+    value_class& method(const char* decl, R (*fn)(Args...))
+    {
+        static_assert(sizeof...(Args) != 0, "Empty parameters");
+
+        method_wrapper_impl<R, Args...>(decl, fn);
+
+        return *this;
+    }
+
 private:
     asIScriptEngine* m_engine;
     const char* m_name;
+
+    template <typename R, typename... Args>
+    void method_wrapper_impl(const char* decl, R (*fn)(Args...))
+    {
+        using args_t = std::tuple<Args...>;
+        constexpr std::size_t arg_count = sizeof...(Args);
+        using first_arg_t = std::tuple_element_t<0, args_t>;
+        using last_arg_t = std::tuple_element_t<sizeof...(Args) - 1, args_t>;
+
+        constexpr bool obj_last = detail::is_this_arg<std::remove_cvref_t<last_arg_t>, T>;
+        constexpr bool obj_first = detail::is_this_arg<std::remove_cvref_t<first_arg_t>, T> && arg_count != 1;
+
+        static_assert(!(obj_last && obj_first), "Ambiguous object parameter");
+        static_assert(obj_last || obj_first, "Missing object parameter");
+
+        if constexpr(obj_last)
+            method(decl, fn, call_conv<asCALL_CDECL_OBJLAST>);
+        else if constexpr(obj_first)
+            method(decl, fn, call_conv<asCALL_CDECL_OBJFIRST>);
+    }
 };
 } // namespace asbind20
 
--- test/test_bind.cpp
+++ test/test_bind.cpp
@@ -26,6 +26,16 @@ public:
 
     int value = 0;
 };
+
+void add_obj_last(int val, my_value_class* this_)
+{
+    this_->value += val;
+}
+
+void mul_obj_first(my_value_class* this_, int val)
+{
+    this_->value *= val;
+}
 } // namespace test_bind
 
 using asbind_test::asbind_test_suite;
@@ -39,16 +49,32 @@ TEST_F(asbind_test_suite, value_class)
     asbind20::value_class<my_value_class>(engine, "my_value_class")
         .register_basic_methods()
         .method("void set_val(int)", &my_value_class::set_val)
-        .method("int get_val() const", &my_value_class::get_val);
+        .method("int get_val() const", &my_value_class::get_val)
+        .method("void add(int val)", test_bind::add_obj_last)
+        .method("void mul(int val)", test_bind::mul_obj_first);
 
     asIScriptModule* m = engine->GetModule("test_value_class", asGM_ALWAYS_CREATE);
 
     m->AddScriptSection(
         "test_value_class.as",
-        "int test()"
+        "int test_1()"
         "{"
         "my_value_class val;"
         "val.set_val(42);"
+        "return val.get_val();"
+        "}"
+        "int test_2()"
+        "{"
+        "my_value_class val;"
+        "val.set_val(182375);"
+        "val.add(1);"
+        "return val.get_val();"
+        "}"
+        "int test_3()"
+        "{"
+        "my_value_class val;"
+        "val.set_val(2);"
+        "val.mul(3);"
         "return val.get_val();"
         "}"
     );
@@ -56,8 +82,14 @@ TEST_F(asbind_test_suite, value_class)
 
     asIScriptContext* ctx = engine->CreateContext();
     {
-        auto test = asbind20::script_function<int()>(m->GetFunctionByName("test"));
-        EXPECT_EQ(test(ctx), 42);
+        auto test_1 = asbind20::script_function<int()>(m->GetFunctionByName("test_1"));
+        EXPECT_EQ(test_1(ctx), 42);
+
+        auto test_2 = asbind20::script_function<int()>(m->GetFunctionByName("test_2"));
+        EXPECT_EQ(test_2(ctx), 182376);
+
+        auto test_3 = asbind20::script_function<int()>(m->GetFunctionByName("test_3"));
+        EXPECT_EQ(test_3(ctx), 6);
     }
     ctx->Release();
 }
