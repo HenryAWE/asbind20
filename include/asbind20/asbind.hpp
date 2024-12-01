3f8b5e856c4a34e25f2e2f6b48b263ec05aaf85b
AWE Henry <mouth11or12@outlook.com>
2024-12-01 17:43:35 +0800

    Update version number to 1.0.0

--- include/asbind20/asbind.hpp
+++ include/asbind20/asbind.hpp
@@ -5,13 +5,13 @@
 
 // clang-format off: Used by CMakeLists.txt for parsing version
 
-#define ASBIND20_VERSION_MAJOR 0
-#define ASBIND20_VERSION_MINOR 1
+#define ASBIND20_VERSION_MAJOR 1
+#define ASBIND20_VERSION_MINOR 0
 #define ASBIND20_VERSION_PATCH 0
 
 // clang-format on
 
-#define ASBIND20_VERSION_STRING "0.1.0"
+#define ASBIND20_VERSION_STRING "1.0.0"
 
 #include "utility.hpp"
 #include "bind.hpp"
