e872a3e218344b5d1a6f2d7b5625880ca387ee06
AWE Henry <mouth11or12@outlook.com>
2025-02-25 13:51:47 +0800

    IO: memory wrapper

--- include/asbind20/io.hpp
+++ include/asbind20/io.hpp
@@ -4,6 +4,9 @@
 #pragma once
 
 #include "detail/include_as.hpp"
+#include <cassert>
+#include <cstring>
+#include <algorithm>
 #include <iostream>
 
 namespace asbind20
@@ -68,6 +71,84 @@ namespace io
     private:
         std::istream* m_is;
     };
+
+    /**
+     * @brief Copy content from binary stream to an output interator
+     *
+     * @tparam OutputIterator Output iterator
+     * @tparam ValueType Single byte value type
+     */
+    template <typename OutputIterator, typename ValueType = std::byte>
+    class copy_to : public AS_NAMESPACE_QUALIFIER asIBinaryStream
+    {
+    public:
+        static_assert(sizeof(ValueType) == 1);
+        static_assert(std::output_iterator<OutputIterator, ValueType>);
+
+        explicit copy_to(OutputIterator it) noexcept
+            : m_out(std::move(it)) {}
+
+        int Read(void* ptr, AS_NAMESPACE_QUALIFIER asUINT size) override
+        {
+            (void)ptr;
+            (void)size;
+            return AS_NAMESPACE_QUALIFIER asERROR;
+        }
+
+        int Write(const void* ptr, AS_NAMESPACE_QUALIFIER asUINT size) override
+        {
+            using input_iterator_type = std::add_const_t<ValueType>*;
+            m_out = std::copy_n((input_iterator_type)ptr, size, m_out);
+
+            return AS_NAMESPACE_QUALIFIER asSUCCESS;
+        }
+
+        OutputIterator out() const
+        {
+            return m_out;
+        }
+
+    private:
+        OutputIterator m_out;
+    };
+
+    class memory_reader final : public AS_NAMESPACE_QUALIFIER asIBinaryStream
+    {
+    public:
+        memory_reader(const void* mem, std::size_t sz) noexcept
+            : m_ptr(static_cast<const std::byte*>(mem)), m_avail(sz)
+        {
+            assert(mem != nullptr);
+        }
+
+        int Read(void* ptr, AS_NAMESPACE_QUALIFIER asUINT size) override
+        {
+            if(size > m_avail)
+                return AS_NAMESPACE_QUALIFIER asOUT_OF_MEMORY;
+            std::memcpy(ptr, m_ptr, size);
+            m_avail -= size;
+            m_ptr += size;
+
+            return AS_NAMESPACE_QUALIFIER asSUCCESS;
+        }
+
+        int Write(const void* ptr, AS_NAMESPACE_QUALIFIER asUINT size) override
+        {
+            (void)ptr;
+            (void)size;
+            return AS_NAMESPACE_QUALIFIER asERROR;
+        }
+
+    private:
+        const std::byte* m_ptr;
+        std::size_t m_avail;
+    };
+
+    struct load_byte_code_result
+    {
+        int r;
+        bool debug_info_stripped;
+    };
 } // namespace io
 
 inline int save_byte_code(
@@ -80,12 +161,35 @@ inline int save_byte_code(
     return m->SaveByteCode(&wrapper, strip_debug_info);
 }
 
-inline std::pair<int, bool> load_byte_code(
+template <typename OutputIteratorValueType = std::byte>
+int save_byte_code(
+    std::output_iterator<OutputIteratorValueType> auto out,
+    AS_NAMESPACE_QUALIFIER asIScriptModule* m,
+    bool strip_debug_info = false
+)
+{
+    io::copy_to<decltype(out), OutputIteratorValueType> wrapper(std::move(out));
+    return m->SaveByteCode(&wrapper, strip_debug_info);
+}
+
+inline io::load_byte_code_result load_byte_code(
     std::istream& is,
     AS_NAMESPACE_QUALIFIER asIScriptModule* m
 )
 {
     io::istream_wrapper wrapper(is);
+    bool debug_info_stripped;
+    int r = m->LoadByteCode(&wrapper, &debug_info_stripped);
+    return {r, debug_info_stripped};
+}
+
+inline io::load_byte_code_result load_byte_code(
+    const void* mem,
+    std::size_t size,
+    AS_NAMESPACE_QUALIFIER asIScriptModule* m
+)
+{
+    io::memory_reader wrapper(mem, size);
     bool debug_info_stripped;
     int r = m->LoadByteCode(&wrapper, &debug_info_stripped);
     return {r, debug_info_stripped};
--- test/test_io.cpp
+++ test/test_io.cpp
@@ -39,6 +39,44 @@ TEST(test_io, iostream_wrapper)
     }
 }
 
+TEST(test_io, memory_wrapper)
+{
+    std::vector<std::byte> buf;
+
+    {
+        auto engine = asbind20::make_script_engine();
+        asbind_test::setup_message_callback(engine);
+
+        auto* m = engine->GetModule("test", AS_NAMESPACE_QUALIFIER asGM_ALWAYS_CREATE);
+        m->AddScriptSection("test.as", "int f(int add) { return 1000 + add; }");
+        ASSERT_GE(m->Build(), 0);
+
+        ASSERT_GE(
+            asbind20::save_byte_code(std::back_inserter(buf), m, true), 0
+        );
+    }
+
+    {
+        auto engine = asbind20::make_script_engine();
+        asbind_test::setup_message_callback(engine);
+
+        auto* m = engine->GetModule("test", AS_NAMESPACE_QUALIFIER asGM_ALWAYS_CREATE);
+        {
+            auto [r, debug_info_stripped] = asbind20::load_byte_code(buf.data(), buf.size(), m);
+            ASSERT_GE(r, 0);
+            EXPECT_TRUE(debug_info_stripped);
+        }
+
+        auto* getval = m->GetFunctionByDecl("int f(int)");
+        ASSERT_NE(getval, nullptr);
+
+        asbind20::request_context ctx(engine);
+        auto result = asbind20::script_invoke<int>(ctx, getval, 13);
+        ASSERT_TRUE(asbind_test::result_has_value(result));
+        EXPECT_EQ(result.value(), 1013);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
