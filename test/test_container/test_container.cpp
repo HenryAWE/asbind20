2fe4f07917b8e41233e4483d0a0353f3924d83e0
AWE Henry <mouth11or12@outlook.com>
2025-03-16 16:04:35 +0800

    Abandon development of sequence container wrappers

--- test/test_container/test_container.cpp
+++ test/test_container/test_container.cpp
@@ -4,8 +4,5 @@
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
-#ifdef ASBIND20_TEST_SKIP_SEQUENCE_TEST
-    std::cerr << "ASBIND20_TEST_SKIP_SEQUENCE_TEST defined" << std::endl;
-#endif
     return RUN_ALL_TESTS();
 }
--- test/test_container/test_container.hpp
+++ test/test_container/test_container.hpp
@@ -5,9 +5,3 @@
 #include <shared_test_lib.hpp>
 
 // IWYU pragma: end_exports
-
-#if defined(__GNUC__) && !defined(__clang__)
-#    if __GNUC__ <= 12
-#        define ASBIND20_TEST_SKIP_SEQUENCE_TEST
-#    endif
-#endif
