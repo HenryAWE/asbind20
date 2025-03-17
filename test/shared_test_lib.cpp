0c70315e26596c7e538584ae5435dddfcbfdaaa6
AWE Henry <mouth11or12@outlook.com>
2025-03-17 20:39:48 +0800

    CMake: enable tests to load external scripts on Emscripten

--- test/shared_test_lib.cpp
+++ test/shared_test_lib.cpp
@@ -159,7 +159,7 @@ void asbind_test_suite::run_file(
 )
 {
     if(!std::filesystem::exists(filename))
-        GTEST_SKIP() << "File not found: " << filename;
+        FAIL() << "File not found: " << filename;
 
     asIScriptModule* m = m_engine->GetModule("run_file", asGM_ALWAYS_CREATE);
 
