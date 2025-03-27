ce487600d961acbd4558d02e27978ca8dfd5872e
AWE Henry <mouth11or12@outlook.com>
2025-03-27 14:00:10 +0800

    Fix for Read the Docs build

--- doc/conf.py
+++ doc/conf.py
@@ -1,3 +1,5 @@
+import subprocess, os
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -27,5 +29,15 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # Breathe Configuration
-breathe_projects = { "asbind20": "../build/doxygen-output/xml" }
+breathe_projects = {}
 breathe_default_project = "asbind20"
+
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+    os.mkdir("doxygen-output")
+    subprocess.call("doxygen", shell=True)
+    breathe_projects["asbind20"] = "doxygen-output/xml"
+else:
+    breathe_projects["asbind20"] = "../build/doxygen-output/xml"
