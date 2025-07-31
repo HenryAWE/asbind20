aa9bbf14c9d60a82c204fb4602859cce8149eca0
AWE Henry <mouth11or12@outlook.com>
2025-07-31 19:57:27 +0800

    Bump version number to 1.7.1

--- include/asbind20/asbind.hpp
+++ include/asbind20/asbind.hpp
@@ -15,11 +15,11 @@
 
 #define ASBIND20_VERSION_MAJOR 1
 #define ASBIND20_VERSION_MINOR 7
-#define ASBIND20_VERSION_PATCH 0
+#define ASBIND20_VERSION_PATCH 1
 
 // clang-format on
 
-#define ASBIND20_VERSION_STRING "1.7.0"
+#define ASBIND20_VERSION_STRING "1.7.1"
 
 // IWYU pragma: begin_exports
 
