337317c929ce298ca9cba320ca7d69a771ed9f17
AWE Henry <mouth11or12@outlook.com>
2025-10-08 17:29:03 +0800

    Move `enum_` into a separated header

--- include/asbind20/bind.hpp
+++ include/asbind20/bind.hpp
@@ -17,6 +17,7 @@
 
 #include "bind/global.hpp"
 #include "bind/class.hpp"
+#include "bind/enum.hpp"
 
 // IWYU pragma: end_exports
 
@@ -184,94 +185,6 @@ public:
         [[maybe_unused]]
         int r = 0;
         r = m_engine->RegisterFuncdef(full_decl.data());
-        assert(r >= 0);
-
-        return *this;
-    }
-
-    [[nodiscard]]
-    auto get_engine() const noexcept
-        -> AS_NAMESPACE_QUALIFIER asIScriptEngine*
-    {
-        return m_engine;
-    }
-
-    [[nodiscard]]
-    const std::string& get_name() const noexcept
-    {
-        return m_name;
-    }
-
-private:
-    AS_NAMESPACE_QUALIFIER asIScriptEngine* m_engine;
-    std::string m_name;
-};
-
-template <typename Enum>
-requires(std::is_enum_v<Enum> || std::integral<Enum>)
-class enum_
-{
-public:
-    using enum_type = Enum;
-
-    enum_() = delete;
-    enum_(const enum_&) = default;
-
-    enum_(
-        AS_NAMESPACE_QUALIFIER asIScriptEngine* engine, std::string name
-    )
-        : m_engine(engine), m_name(std::move(name))
-    {
-        [[maybe_unused]]
-        int r = 0;
-        r = m_engine->RegisterEnum(m_name.c_str());
-        assert(r >= 0);
-    }
-
-    template <std::convertible_to<std::string_view> StringView>
-    enum_(
-        AS_NAMESPACE_QUALIFIER asIScriptEngine* engine,
-        StringView name
-    )
-        : enum_(
-              engine,
-              std::string(static_cast<std::string_view>(name))
-          )
-    {}
-
-    enum_& value(Enum val, std::string_view decl)
-    {
-        [[maybe_unused]]
-        int r = with_cstr(
-            &AS_NAMESPACE_QUALIFIER asIScriptEngine::RegisterEnumValue,
-            m_engine,
-            m_name,
-            decl,
-            static_cast<int>(val)
-        );
-        assert(r >= 0);
-
-        return *this;
-    }
-
-    /**
-     * @brief Registering an enum value. Its declaration will be generated from its name in C++.
-     *
-     * @note This function has some limitations. @sa static_enum_name
-     *
-     * @tparam Value Enum value
-     */
-    template <Enum Value>
-    requires(std::is_enum_v<Enum>)
-    enum_& value()
-    {
-        [[maybe_unused]]
-        int r = 0;
-        r = m_engine->RegisterEnumValue(
-            m_name.c_str(),
-            meta::fixed_enum_name<Value>().c_str(),
-            static_cast<int>(Value)
-        );
         assert(r >= 0);
 
         return *this;
