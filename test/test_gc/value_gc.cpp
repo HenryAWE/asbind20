7edc6b44d7c94f731baae81ff424bbaebbad8c20
AWE Henry <mouth11or12@outlook.com>
2025-11-18 01:25:35 +0800

    Update test for GC and value type

--- test/test_gc/value_gc.cpp
+++ test/test_gc/value_gc.cpp
@@ -1,18 +1,155 @@
 #include <gtest/gtest.h>
 #include <asbind_test/framework.hpp>
 #include <asbind20/asbind.hpp>
-#include <asbind20/ext/vocabulary.hpp>
+
+namespace test_gc
+{
+static bool val_gc_template_callback(
+    AS_NAMESPACE_QUALIFIER asITypeInfo* ti, bool& no_gc
+)
+{
+    int subtype_id = ti->GetSubTypeId();
+    if(asbind20::is_void_type(subtype_id))
+        return false;
+
+    if(asbind20::is_primitive_type(subtype_id))
+        no_gc = true;
+    else
+        no_gc = !asbind20::type_requires_gc(ti->GetSubType());
+
+    return true;
+}
+
+class val_gc
+{
+public:
+    val_gc(AS_NAMESPACE_QUALIFIER asITypeInfo* ti)
+        : m_ti(ti) {}
+
+    val_gc(const val_gc& other)
+        : m_ti(other.m_ti)
+    {
+        if(other.has_value())
+        {
+            int type_id = m_ti->GetSubTypeId();
+            bool no_ex = asbind20::container::single::copy_construct(
+                m_data,
+                m_ti->GetEngine(),
+                type_id,
+                asbind20::container::single::data_address(m_data, type_id)
+            );
+            if(!no_ex) [[unlikely]]
+                return;
+            m_has_value = no_ex;
+        }
+    }
+
+    ~val_gc()
+    {
+        reset();
+    }
+
+    bool has_value() const noexcept
+    {
+        return m_has_value;
+    }
+
+    void assign(const void* val)
+    {
+        EXPECT_NE(m_ti, nullptr);
+
+        if(has_value())
+        {
+            asbind20::container::single::copy_assign_from(
+                m_data, m_ti->GetEngine(), m_ti->GetSubTypeId(), val
+            );
+        }
+        else
+        {
+            m_has_value = asbind20::container::single::copy_construct(
+                m_data, m_ti->GetEngine(), m_ti->GetSubTypeId(), val
+            );
+        }
+    }
+
+    const void* value() const
+    {
+        if(!has_value())
+        {
+            asbind20::set_script_exception("bad optional access");
+            return nullptr;
+        }
+
+        return asbind20::container::single::data_address(
+            m_data, m_ti->GetSubTypeId()
+        );
+    }
+
+    void reset()
+    {
+        if(!m_has_value)
+            return;
+
+        asbind20::container::single::destroy(
+            m_data, m_ti->GetEngine(), m_ti->GetSubTypeId()
+        );
+        m_has_value = false;
+    }
+
+    void enum_refs(AS_NAMESPACE_QUALIFIER asIScriptEngine* engine)
+    {
+        EXPECT_TRUE((m_ti->GetFlags() & AS_NAMESPACE_QUALIFIER asOBJ_GC))
+            << "m_ti->GetFlags(): " << m_ti->GetFlags();
+        EXPECT_EQ(engine, m_ti->GetEngine());
+
+        asbind20::container::single::enum_refs(
+            m_data, m_ti->GetSubType()
+        );
+    }
+
+    void release_refs(AS_NAMESPACE_QUALIFIER asIScriptEngine* engine)
+    {
+        EXPECT_EQ(engine, m_ti->GetEngine());
+        reset();
+    }
+
+    template <bool UseGeneric>
+    friend void register_val_gc(AS_NAMESPACE_QUALIFIER asIScriptEngine* engine);
+
+private:
+    asbind20::script_typeinfo m_ti;
+
+    asbind20::container::single::data_type m_data;
+    bool m_has_value = false;
+};
+
+template <bool UseGeneric>
+void register_val_gc(AS_NAMESPACE_QUALIFIER asIScriptEngine* engine)
+{
+    using namespace asbind20;
+
+    template_value_class<val_gc, UseGeneric>(engine, "val_gc<T>", AS_NAMESPACE_QUALIFIER asOBJ_GC)
+        .template_callback(fp<&val_gc_template_callback>)
+        .default_constructor()
+        .destructor()
+        .enum_refs(fp<&val_gc::enum_refs>)
+        .release_refs(fp<&val_gc::release_refs>)
+        .property("const bool has_value", &val_gc::m_has_value)
+        .method("const T& get_value() const property", fp<overload_cast<>(&val_gc::value, const_)>)
+        .method("void set_value(const T&in) property", fp<&val_gc::assign>);
+}
+} // namespace test_gc
 
 static constexpr char optional_gc_test_script[] = R"(bool test0()
 {
     output_stat();
-    optional<int> o = nullopt;
+    val_gc<int> o;
     return !o.has_value;
 }
 
 class foo
 {
-    optional<foo@> ref = nullopt;
+    val_gc<foo@> ref;
 }
 
 bool test1()
@@ -39,9 +176,8 @@ public:
         m_engine = asbind20::make_script_engine();
         asbind_test::setup_message_callback(m_engine, true);
 
-        namespace ext = asbind20::ext;
         asbind_test::setup_script_assertion(m_engine);
-        ext::register_script_optional(m_engine, UseGeneric);
+        register_val_gc<UseGeneric>(m_engine);
 
         asbind20::global<true>(m_engine)
             .function(
@@ -52,12 +188,13 @@ public:
                     ASSERT_TRUE(ctx);
 
                     auto* f = ctx->GetFunction();
+                    const char* section = "(null)";
+                    int line = ctx->GetLineNumber(0, nullptr, &section);
                     std::cerr
                         << '['
-                        << asbind20::debugging::get_function_section_name(f)
-                        << ':'
-                        << f->GetName()
-                        << "] ";
+                        << section << ':' << line
+                        << " (" << f->GetName()
+                        << ")] ";
                     asbind_test::output_gc_statistics(
                         std::cerr,
                         ctx->GetEngine(),
@@ -107,6 +244,21 @@ public:
             EXPECT_TRUE(asbind_test::result_has_value(result));
             EXPECT_TRUE(result.value());
         }
+
+        std::cerr << "After tests\n";
+        asbind_test::output_gc_statistics(
+            std::cerr, m_engine, ';'
+        );
+
+        m_engine->GarbageCollect();
+
+        auto collected = asbind20::debugging::get_gc_statistics(m_engine);
+        EXPECT_EQ(collected.current_size, 0);
+
+        std::cerr << "Collected: ";
+        asbind_test::output_gc_statistics(
+            std::cerr, m_engine, ';'
+        );
     }
 
 private:
