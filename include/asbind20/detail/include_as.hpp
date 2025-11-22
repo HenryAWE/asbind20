740feb4847da9d1ae94523e701e6adfde697e80f
HenryAWE <mouth11or12@outlook.com>
2025-11-22 21:27:16 +0800

    MacOS support (#38)
    - Fix warning on Apple Clang
    - CI for Mac
    - Update `.gitignore`

--- include/asbind20/detail/include_as.hpp
+++ include/asbind20/detail/include_as.hpp
@@ -10,9 +10,18 @@
 
 #pragma once
 
+#ifdef __clang__
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-Wunused-parameter"
+#endif
+
 #ifndef ANGELSCRIPT_H
 // Avoid having to inform include path if header is already included before
 #    include <angelscript.h> // IWYU pragma: export
+#endif
+
+#ifdef __clang__
+#    pragma clang diagnostic pop
 #endif
 
 #endif
