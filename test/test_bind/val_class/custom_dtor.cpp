0a01e326dd51a27826b8ad9c5ffca03a6a36df4f
AWE Henry <mouth11or12@outlook.com>
2026-01-18 04:01:21 +0800

    Update tests for custom destructor

--- test/test_bind/val_class/custom_dtor.cpp
+++ test/test_bind/val_class/custom_dtor.cpp
@@ -2,8 +2,6 @@
 
 namespace test_bind
 {
-static int global_counter = 0;
-
 class dtor_tester
 {
     [[maybe_unused]]
@@ -20,28 +18,31 @@ public:
 
     ~dtor_tester()
     {
-        ++scoped_counter;
+        ++dtor_counter;
     }
 
-    static inline int scoped_counter = 0;
+    static inline int dtor_counter = 0;
+    static inline int mfn_counter = 0;
+
+    // Member function for self-deletion
+    void self_delete()
+    {
+        ++mfn_counter;
+        std::destroy_at(this);
+    }
 };
 
-static void external_dtor(dtor_tester* this_)
-{
-    ++global_counter;
-    this_->~dtor_tester();
-}
-
 template <bool UseGeneric>
-class custom_dtor_suite : public ::testing::Test
+class custom_dtor_suite_base : public ::testing::Test
 {
 protected:
-   static void reset_counters()
+    virtual void reset_counters() const
     {
-        global_counter = 0;
-        dtor_tester::scoped_counter = 0;
+        dtor_tester::dtor_counter = 0;
+        dtor_tester::mfn_counter = 0;
     }
 
+public:
     auto setup_dtor_tester()
     {
         using namespace asbind20;
@@ -58,7 +59,6 @@ protected:
         return c;
     }
 
-public:
     void SetUp() override
     {
         reset_counters();
@@ -97,6 +97,77 @@ public:
 
         return m;
     }
+};
+
+template <bool UseGeneric>
+class custom_dtor_mfn_suite : public custom_dtor_suite_base<UseGeneric>
+{
+    using my_base = custom_dtor_suite_base<UseGeneric>;
+
+public:
+    using my_base::compile_module;
+    using my_base::engine;
+
+    void run_dtor_test() const
+    {
+        auto* m = compile_module();
+
+        auto* f = m->GetFunctionByName("test");
+        ASSERT_NE(f, nullptr);
+
+        asbind20::request_context ctx(engine);
+        auto result = asbind20::script_invoke<void>(ctx, f);
+        ASBIND_TEST_ASSERT_INVOKE_RESULT(result);
+        EXPECT_EQ(dtor_tester::dtor_counter, 1);
+        EXPECT_EQ(dtor_tester::mfn_counter, 1);
+    }
+};
+} // namespace test_bind
+
+using CustomDestructorMFNNative = test_bind::custom_dtor_mfn_suite<false>;
+using CustomDestructorMFNGeneric = test_bind::custom_dtor_mfn_suite<true>;
+
+TEST_F(CustomDestructorMFNNative, RunDtorTest)
+{
+    using test_bind::dtor_tester;
+    setup_dtor_tester()
+        .destructor_function(asbind20::fp<&dtor_tester::self_delete>);
+    run_dtor_test();
+}
+
+TEST_F(CustomDestructorMFNGeneric, RunDtorTest)
+{
+    using test_bind::dtor_tester;
+    setup_dtor_tester()
+        .destructor_function(asbind20::fp<&dtor_tester::self_delete>);
+    run_dtor_test();
+}
+
+namespace test_bind
+{
+static int global_counter = 0;
+
+static void external_dtor(dtor_tester* this_)
+{
+    ++global_counter;
+    this_->~dtor_tester();
+}
+
+template <bool UseGeneric>
+class custom_dtor_external_suite : public custom_dtor_suite_base<UseGeneric>
+{
+    using my_base = custom_dtor_suite_base<UseGeneric>;
+
+protected:
+    void reset_counters() const override
+    {
+        my_base::reset_counters();
+        global_counter = 0;
+    }
+
+public:
+    using my_base::compile_module;
+    using my_base::engine;
 
     void run_dtor_test() const
     {
@@ -109,15 +180,17 @@ public:
         auto result = asbind20::script_invoke<void>(ctx, f);
         ASBIND_TEST_ASSERT_INVOKE_RESULT(result);
         EXPECT_EQ(global_counter, 1);
-        EXPECT_EQ(dtor_tester::scoped_counter, 1);
+        EXPECT_EQ(dtor_tester::dtor_counter, 1);
+        EXPECT_EQ(dtor_tester::mfn_counter, 0)
+            << "This should be untouched";
     }
 };
 } // namespace test_bind
 
-using CustomDestructorNative = test_bind::custom_dtor_suite<false>;
-using CustomDestructorGeneric = test_bind::custom_dtor_suite<true>;
+using CustomDestructorExternalNative = test_bind::custom_dtor_external_suite<false>;
+using CustomDestructorExternalGeneric = test_bind::custom_dtor_external_suite<true>;
 
-TEST_F(CustomDestructorNative, RunDtorTest)
+TEST_F(CustomDestructorExternalNative, RunDtorTest)
 {
     using test_bind::external_dtor;
     setup_dtor_tester()
@@ -125,7 +198,7 @@ TEST_F(CustomDestructorNative, RunDtorTest)
     run_dtor_test();
 }
 
-TEST_F(CustomDestructorGeneric, RunDtorTest)
+TEST_F(CustomDestructorExternalGeneric, RunDtorTest)
 {
     using test_bind::external_dtor;
     setup_dtor_tester()
@@ -135,12 +208,12 @@ TEST_F(CustomDestructorGeneric, RunDtorTest)
 
 namespace test_bind
 {
-class counter_class
+class aux_counter
 {
 public:
     int counter = 0;
 
-    void dtor_as_global(dtor_tester* obj)
+    void aux_dtor(dtor_tester* obj)
     {
         ++counter;
         obj->~dtor_tester();
@@ -148,15 +221,25 @@ public:
 };
 
 template <bool UseGeneric>
-class custom_dtor_suite_thiscall : public custom_dtor_suite<UseGeneric>
+class custom_dtor_suite_aux : public custom_dtor_suite_base<UseGeneric>
 {
-    using my_base = custom_dtor_suite<UseGeneric>;
+    using my_base = custom_dtor_suite_base<UseGeneric>;
+
+    // Magic number to ensure the value is untouched
+    static constexpr int magic_num = 42;
+
+    void reset_counters() const override
+    {
+        my_base::reset_counters();
+        // This value should be untouched
+        global_counter = magic_num;
+    }
 
 public:
     using my_base::compile_module;
     using my_base::engine;
 
-    void run_dtor_test(counter_class& instance)
+    void run_dtor_test(const aux_counter& instance)
     {
         auto* m = compile_module();
 
@@ -167,30 +250,32 @@ public:
         auto result = asbind20::script_invoke<void>(ctx, f);
         ASBIND_TEST_ASSERT_INVOKE_RESULT(result);
         EXPECT_EQ(instance.counter, 1);
-        EXPECT_EQ(dtor_tester::scoped_counter, 1);
+        EXPECT_EQ(dtor_tester::dtor_counter, 1);
+        EXPECT_EQ(global_counter, magic_num)
+            << "This should be untouched";
     }
 };
 } // namespace test_bind
 
-using CustomDestructorThiscallNative = test_bind::custom_dtor_suite_thiscall<false>;
-using CustomDestructorThiscallGeneric = test_bind::custom_dtor_suite_thiscall<true>;
+using CustomDestructorAuxNative = test_bind::custom_dtor_suite_aux<false>;
+using CustomDestructorAuxGeneric = test_bind::custom_dtor_suite_aux<true>;
 
-TEST_F(CustomDestructorThiscallNative, RunDtorTestThiscall)
+TEST_F(CustomDestructorAuxNative, RunDtorTest)
 {
     using namespace asbind20;
-    using test_bind::counter_class;
-    counter_class instance{};
+    using test_bind::aux_counter;
+    aux_counter instance{};
     setup_dtor_tester()
-        .destructor_function(fp<&counter_class::dtor_as_global>, auxiliary(instance));
+        .destructor_function(fp<&aux_counter::aux_dtor>, auxiliary(instance));
     run_dtor_test(instance);
 }
 
-TEST_F(CustomDestructorThiscallGeneric, RunDtorTestThiscall)
+TEST_F(CustomDestructorAuxGeneric, RunDtorTest)
 {
     using namespace asbind20;
-    using test_bind::counter_class;
-    counter_class instance{};
+    using test_bind::aux_counter;
+    aux_counter instance{};
     setup_dtor_tester()
-        .destructor_function(fp<&counter_class::dtor_as_global>, auxiliary(instance));
+        .destructor_function(fp<&aux_counter::aux_dtor>, auxiliary(instance));
     run_dtor_test(instance);
 }
