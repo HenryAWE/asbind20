c9743545545146f1826070e87a48fecb271540e2
AWE Henry <mouth11or12@outlook.com>
2026-03-22 19:58:57 +0800

    Update implementation

--- include/asbind20/bind/listener.hpp
+++ include/asbind20/bind/listener.hpp
@@ -23,64 +23,36 @@ public:
 
     listener_traits() = delete;
 
-    template <typename BindingGenerator>
-    static void on_function(Listener& listener, BindingGenerator&& gen, int id)
-    {
-        constexpr bool has_func = requires() {
-            listener.on_function(std::forward<BindingGenerator>(gen), id);
-        };
-        if constexpr(has_func)
-            listener.on_function(std::forward<BindingGenerator>(gen), id);
-        else
-            default_fallback(id);
+#define ASBIND20_LISTENER_GENERAL_FUNC_IMPL(sig, name)                  \
+    template <typename BindingGenerator>                                \
+    static void sig(Listener& listener, BindingGenerator&& gen, int id) \
+    {                                                                   \
+        constexpr bool has_func = requires() {                          \
+            listener.sig(std::forward<BindingGenerator>(gen), id);      \
+        };                                                              \
+        if constexpr(has_func)                                          \
+            listener.sig(std::forward<BindingGenerator>(gen), id);      \
+        else                                                            \
+            default_fallback(id, "bad " name);                          \
     }
 
-    template <typename BindingGenerator>
-    static void on_global_property(Listener& listener, BindingGenerator&& gen, int id)
-    {
-        constexpr bool has_func = requires() {
-            listener.on_global_property(std::forward<BindingGenerator>(gen), id);
-        };
-        if constexpr(has_func)
-            listener.on_global_property(std::forward<BindingGenerator>(gen), id);
-        else
-            default_fallback(id);
-    }
+    ASBIND20_LISTENER_GENERAL_FUNC_IMPL(on_function, "function")
+    ASBIND20_LISTENER_GENERAL_FUNC_IMPL(on_global_property, "global property")
+    ASBIND20_LISTENER_GENERAL_FUNC_IMPL(on_funcdef, "funcdef")
+    ASBIND20_LISTENER_GENERAL_FUNC_IMPL(on_typedef, "typedef")
 
-    template <typename BindingGenerator>
-    static void on_funcdef(Listener& listener, BindingGenerator&& gen, int id)
-    {
-        constexpr bool has_func = requires() {
-            listener.on_funcdef(std::forward<BindingGenerator>(gen), id);
-        };
-        if constexpr(has_func)
-            listener.on_funcdef(std::forward<BindingGenerator>(gen), id);
-        else
-            default_fallback(id);
-    }
-
-    template <typename BindingGenerator>
-    static void on_typedef(Listener& listener, BindingGenerator&& gen, int id)
-    {
-        constexpr bool has_func = requires() {
-            listener.on_typedef(std::forward<BindingGenerator>(gen), id);
-        };
-        if constexpr(has_func)
-            listener.on_typedef(std::forward<BindingGenerator>(gen), id);
-        else
-            default_fallback(id);
-    }
+#undef ASBIND20_LISTENER_GENERAL_FUNC_IMPL
 
 private:
-    static void default_fallback(int val)
+    static void default_fallback(int val, const char* what)
     {
-        if(val >= 0)
+        if(val >= 0) [[likely]]
             return;
 
         // Report error
         auto code = static_cast<AS_NAMESPACE_QUALIFIER asERetCodes>(val);
         ::asbind20::detail::throw_<std::system_error>(
-            make_error_code(code)
+            make_error_code(code), what
         );
     }
 };
