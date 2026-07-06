f0072378ab192043f61d00d9edc3536faa6419b5
AWE Henry <mouth11or12@outlook.com>
2026-07-07 06:23:30 +0800

    Bump version number to 2.1

--- include/asbind20/asbind.hpp
+++ include/asbind20/asbind.hpp
@@ -14,12 +14,12 @@
 // clang-format off: Used by CMakeLists.txt for parsing version
 
 #define ASBIND20_VERSION_MAJOR 2
-#define ASBIND20_VERSION_MINOR 0
-#define ASBIND20_VERSION_PATCH 1
+#define ASBIND20_VERSION_MINOR 1
+#define ASBIND20_VERSION_PATCH 0
 
 // clang-format on
 
-#define ASBIND20_VERSION_STRING "2.0.1"
+#define ASBIND20_VERSION_STRING "2.1.0"
 
 // IWYU pragma: begin_exports
 
