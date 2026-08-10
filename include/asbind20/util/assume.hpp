65e319c371e7372b4cafe5897730ebf0f784b3de
AWE Henry <mouth11or12@outlook.com>
2026-08-11 07:25:03 +0800

    Try fixing warning

--- include/asbind20/util/assume.hpp
+++ include/asbind20/util/assume.hpp
@@ -6,6 +6,13 @@
 #if __has_cpp_attribute(assume)
 #    define ASBIND20_ASSUME(expr) [[assume(expr)]]
 
+// Clang supports [[assume]] under C++20 with warning
+#    if defined(__clang__)
+#        if __cplusplus < 202302L
+#            pragma clang diagnostic ignored "-Wc++23-attribute-extensions"
+#        endif
+#    endif
+
 // Fallback implementation from P1774R8
 // See: https://wg21.link/P1774
 #elif defined(__clang__)
